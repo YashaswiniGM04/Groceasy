b8683173ce5e38c7f8f36ded2f71a6925b100b8d
Yashaswini <yashaswinigm04@gmail.com>
2023-04-30 21:02:37 +0530

    Initial commit

---
+++ index.js
@@ -0,0 +1,11 @@
+
+const inputField = document.getElementById("input-txt")
+
+const cartBtn = document.getElementById("cart-btn")
+
+cartBtn.addEventListener("click",function(){
+    let input = inputField.value
+    console.log(input)
+})
+
+
