39a7c8a5ac4f6763ee5e24c51894c725b6af4626
Yashaswini <yashaswinigm04@gmail.com>
2023-05-02 00:17:18 +0530

    Final commit

--- index.js
+++ index.js
@@ -1,11 +1,69 @@
 
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-app.js"
+
+import { getDatabase, ref, push, onValue,remove } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-database.js"
+
+
+const appSettings = {
+    databaseURL: "https://groceasy-951aa-default-rtdb.asia-southeast1.firebasedatabase.app/" //database reference URL
+}
+
+const app = initializeApp(appSettings)
+const database = getDatabase(app)
+const itemsInDB = ref(database, "shopItems") // shopItems is the name of the reference 
+
+
 const inputField = document.getElementById("input-txt")
-
 const cartBtn = document.getElementById("cart-btn")
+const shoppingList = document.getElementById("shoppingList")
 
-cartBtn.addEventListener("click",function(){
+
+
+onValue(itemsInDB, function (snapshot) { // itemsInDB is the ref name
+    //snapshot is an object, convert that to array
+    clearShoppingListEl()
+    let cartItems = Object.entries(snapshot.val()) //array
+    for (let i = 0; i < cartItems.length; i++) {
+        let idKeypair = cartItems[i]
+        appendShoppingListEl(idKeypair) // it has both id and value
+    }
+
+    
+}) 
+
+cartBtn.addEventListener("click", function () {
+
     let input = inputField.value
-    console.log(input)
+    push(itemsInDB, input) // itemsInDB is the reference 
+    // appendShoppingListEl(input)
+    clearInputFieldEl()
 })
+
+function clearShoppingListEl(){
+    shoppingList.innerHTML=""
+}
+
+function clearInputFieldEl() {
+    inputField.value = null
+}
+
+function appendShoppingListEl(input) {
+    
+    let listItem = document.createElement("li")
+    listItem.textContent=input[1]
+    shoppingList.append(listItem)
+
+    
+listItem.addEventListener("click",function(){
+    let itemPathInDB = ref(database,`shopItems/${input[0]}`)
+    remove(itemPathInDB)
+})
+
+}
+
+
+
+
+
 
 
